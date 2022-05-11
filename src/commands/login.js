d40582b60e776830ac76300e5f4c29b6cc536abc
Tdroid <tdroid@brq.com>
2022-05-10 23:31:41 -0300

    Sistema de login

--- src/commands/login.js
+++ src/commands/login.js
@@ -1,4 +1,13 @@
-const axios = require('axios')
+const axios = require('axios');
+const c = require('colors');
+
+function UserException(message) {
+	return console.error(c.red('\r' + `[Login] - HighFrame API: error when logging into the instance stream by '${message}'`))
+ }
+
+const headers = {
+  headers: { 'Content-Type': 'application/json' }
+}
 
 module.exports = {
     name: 'login',
@@ -9,8 +18,11 @@ module.exports = {
             template: { generate },
             print: { info },
             prompt,
-            filesystem
+            filesystem: { exists }
           } = toolbox;
+
+          let hasArealyLogin = exists('login.hf.json')
+          if(hasArealyLogin) return info(c.red('❌┃Você já está logado'))
 
           let authorAsk = { 
             type: 'input',
@@ -24,13 +36,69 @@ module.exports = {
             message: '🗝️┃Qual o seu splashBlack de login?'
           }
 
-          const questions = [authorAsk, login]
-          await prompt.ask(questions).then(async (res) => {
-              const { username, splashback } = res
-              axios.post('http://localhost:3001/auth/developers/login', {
+          const question1 = [authorAsk]
+          const question2 = [login];
+
+          let username;
+          let splashback;
+
+          await prompt.ask(question1).then(async (res) => {
+              username = res.username
+
+              await axios.post('http://localhost:3001/api/verify/username', {
+                "username": username,
+              }, { headers }).then(res => {
+
+              }).catch(err => {
+                console.log(err)
+                if(err.response) {
+                  try {
+                    throw new UserException(err.response.data);
+                  } catch {
+                    process.exit(1)
+                  }
+                }
+              })
+          }).then(async () => {
+            await prompt.ask(question2).then(async (res) => {
+              splashback = res.splashback
+
+              await axios.post('http://localhost:3001/api/verify/login', {
                 "splashback": splashback,
                 "username": username,
-              });
-          });
+              }, { headers }).then(async response => {
+                
+                await axios.post('http://localhost:3001/auth/developers/login', {
+                "splashback": splashback,
+                "username": username,
+              }).then(async (result) => {
+                const { id, splashback, username, project, score } = result.data.user
+                console.log(result)
+                await generate({
+                  template: 'login.hf.json.ejs',
+                  target: 'login.hf.json',
+                  props: {
+                      "id": id,
+                      "splashback": splashback,
+                      "username": username,
+                      "project": project,
+                      "score": score
+                  }
+                }).then(() => {
+                  info(c.green(`✅┃Login concluido com sucesso! \n`) + c.yellow(`Guarde o arquivo "login.hf.json" em um local seguro ou adicione em um arquivo .gitignore`))
+                })
+              })
+              }).catch(err => {
+                console.log(err)
+                if(err.response) {
+                  try {
+                    throw new UserException(err.response.data);
+                  } catch {
+                    process.exit(1)
+                  }
+                }
+              })
+            })
+        })
     }
 };
